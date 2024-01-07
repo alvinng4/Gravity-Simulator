030484211b2d642ef6cab5c0fedec2f6f6357f3e
alvinng4 <alvinng324@gmail.com>
2024-01-07 19:02:06 +0800

    Update settings.py
    Added setter for screen width and screen width. Also, added test for them in test_settings.py

--- test/test_settings.py
+++ test/test_settings.py
@@ -1,23 +1,42 @@
 from gravity_sim.settings import Settings
 
 
+def test_settings_screen_width():
+    settings1 = Settings(sun_img_scale=10, img_scale=10, screen_width=1920, screen_height=1080)
+    assert settings1._screen_width == 1920
+    settings2 = Settings(sun_img_scale=10, img_scale=10, screen_width=200.5, screen_height=1080)
+    assert settings2._screen_width == 200.5
+    settings3 = Settings(sun_img_scale=10, img_scale=10, screen_width=-100, screen_height=1080)
+    assert settings3._screen_width == 0
+    settings4 = Settings(sun_img_scale=10, img_scale=10, screen_width=0, screen_height=1080)
+    assert settings4._screen_width == 0
+
+def test_settings_screen_height():
+    settings1 = Settings(sun_img_scale=10, img_scale=10, screen_width=1920, screen_height=1080)
+    assert settings1._screen_height == 1080
+    settings2 = Settings(sun_img_scale=10, img_scale=10, screen_width=1920, screen_height=200.5)
+    assert settings2._screen_height == 200.5
+    settings3 = Settings(sun_img_scale=10, img_scale=10, screen_width=1920, screen_height=-100)
+    assert settings3._screen_height == 0
+    settings4 = Settings(sun_img_scale=10, img_scale=10, screen_width=1920, screen_height=0)
+    assert settings4._screen_height == 0
+
 def test_settings_sun_img_scale():
-    settings1 = Settings(sun_img_scale=10.1)
+    settings1 = Settings(sun_img_scale=10.1, img_scale=10, screen_width=1920, screen_height=1080)
     assert settings1._sun_img_scale == 10.1
-    settings2 = Settings(sun_img_scale=1000)
+    settings2 = Settings(sun_img_scale=1000, img_scale=10, screen_width=1920, screen_height=1080)
     assert settings2._sun_img_scale == 100
-    settings3 = Settings(sun_img_scale=0.1)
+    settings3 = Settings(sun_img_scale=0.1, img_scale=10, screen_width=1920, screen_height=1080)
     assert settings3._sun_img_scale == 0.1
-    settings4 = Settings(sun_img_scale=-10)
+    settings4 = Settings(sun_img_scale=-10, img_scale=10, screen_width=1920, screen_height=1080)
     assert settings4._sun_img_scale == 0
 
 def test_settings_img_scale():
-    settings1 = Settings(img_scale=10.1)
+    settings1 = Settings(sun_img_scale=10, img_scale=10.1, screen_width=1920, screen_height=1080)
     assert settings1._img_scale == 10.1
-    settings2 = Settings(img_scale=1000)
+    settings2 = Settings(sun_img_scale=10, img_scale=1000, screen_width=1920, screen_height=1080)
     assert settings2._img_scale == 100
-    settings3 = Settings(img_scale=0.1)
+    settings3 = Settings(sun_img_scale=10, img_scale=0.1, screen_width=1920, screen_height=1080)
     assert settings3._img_scale == 0.1
-    settings4 = Settings(img_scale=-10)
+    settings4 = Settings(sun_img_scale=-10, img_scale=-10, screen_width=1920, screen_height=1080)
     assert settings4._img_scale == 0
-
