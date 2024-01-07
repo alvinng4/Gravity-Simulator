e9f0ffa5b5c3f491060261a2c94b78a76dcf9cb8
alvinng4 <alvinng324@gmail.com>
2024-01-07 11:30:00 +0800

    Quick fix
    Updated test_settings.py. Fixed a elif in sun_img_scale setter.

--- test/test_settings.py
+++ test/test_settings.py
@@ -1,13 +1,23 @@
 from settings import Settings
 
 
+def test_settings_sun_img_scale():
+    settings1 = Settings(sun_img_scale=10.1)
+    assert settings1._sun_img_scale == 10.1
+    settings2 = Settings(sun_img_scale=1000)
+    assert settings2._sun_img_scale == 100
+    settings3 = Settings(sun_img_scale=0.1)
+    assert settings3._sun_img_scale == 0.1
+    settings4 = Settings(sun_img_scale=-10)
+    assert settings4._sun_img_scale == 0
+
 def test_settings_img_scale():
     settings1 = Settings(img_scale=10.1)
     assert settings1._img_scale == 10.1
     settings2 = Settings(img_scale=1000)
     assert settings2._img_scale == 100
     settings3 = Settings(img_scale=0.1)
-    assert settings3._img_scale == 1
-    settings3 = Settings(img_scale=-10)
-    assert settings3._img_scale == 1
+    assert settings3._img_scale == 0.1
+    settings4 = Settings(img_scale=-10)
+    assert settings4._img_scale == 0
 
