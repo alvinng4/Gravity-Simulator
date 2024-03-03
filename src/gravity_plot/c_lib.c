46d5f525eb84dcb29bf00a124d01f5c8abdedeff
alvinng4 <alvinng324@gmail.com>
2024-03-03 10:33:45 +0800

    Cleaned up and commented the src c_lib files
    Former-commit-id: 34f718f3c6f30722dd48d7ccb80cd13b78ca643d

--- src/gravity_plot/c_lib.c
+++ src/gravity_plot/c_lib.c
@@ -1,7 +1,7 @@
 #include <math.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h> // For testing
+// #include <stdio.h> // For testing
 
 #define real double
 
@@ -13,6 +13,7 @@
     #define min(a,b) ((a) < (b) ? (a) : (b))
 #endif
 
+real vec_norm(const real *vec, int vec_size);
 void acceleration(int objects_count, const real (*x)[3], real (*a)[3], const real *m, real G);
 void euler(
     int objects_count, 
@@ -85,22 +86,38 @@ int rk_embedded(
     int len_sol_time,
     real *sol_time
 );
-real vec_norm(const real *vec, int vec_size);
+
+
+real vec_norm(const real *vec, int vec_size)
+{   
+    real sum = 0.0;
+    if (vec_size == 3) 
+        sum = vec[0] * vec[0] + vec[1] * vec[1] + vec[2] * vec[2];
+    else
+    {
+        for (int i = 0; i < vec_size; i++) sum += vec[i] * vec[i];
+    }
+    return sqrt(sum);
+}
 
 void acceleration(int objects_count, const real (*x)[3], real (*a)[3], const real *m, real G)
 {   
     real R_norm, temp_value, *temp_vec = (real *) malloc(3 * sizeof(real)), *R = (real *) malloc(3 * sizeof(real));
 
-    memset(a, 0, 3 * objects_count * sizeof(real));
+    // Empty the input array
+    memset(a, 0, objects_count * 3 * sizeof(real));
 
     for(int i = 0; i < objects_count; i++)
     {
         for(int j = i + 1; j < objects_count; j++)
         {
+            // Calculate \vec{R} and its norm
             R[0] = x[i][0] - x[j][0];
             R[1] = x[i][1] - x[j][1];
             R[2] = x[i][2] - x[j][2];
             R_norm = vec_norm(R, 3);
+
+            // Calculate the acceleration
             temp_value = G / (R_norm * R_norm * R_norm);
             temp_vec[0] = temp_value * R[0];
             temp_vec[1] = temp_value * R[1];
@@ -118,7 +135,6 @@ void acceleration(int objects_count, const real (*x)[3], real (*a)[3], const rea
     free(R);
 }
 
-
 void euler(
     int objects_count, 
     real (*x)[3], 
@@ -132,14 +148,18 @@ void euler(
     real (*sol_state)[6 * objects_count]
 )
 {   
-    real (*a)[3] = malloc(3 * objects_count * sizeof(real));
+    real (*a)[3] = malloc(objects_count * 3 * sizeof(real));
 
+    // Current progress percentage rounded to int
     int progress_percentage = (int) round(*t / tf * 100);
+
+    // Main Loop
     for(int count = (int) round(*t / dt); count < npts; count++)
     {   
         acceleration(objects_count, x, a, m, G);
         for (int j = 0; j < objects_count; j++)
         {
+            // Calculation
             x[j][0] += v[j][0] * dt;
             x[j][1] += v[j][1] * dt;
             x[j][2] += v[j][2] * dt;
@@ -155,8 +175,9 @@ void euler(
             sol_state[count + 1][objects_count * 3 + j * 3 + 1] = v[j][1];
             sol_state[count + 1][objects_count * 3 + j * 3 + 2] = v[j][2];
         }    
-            
         *t += dt;
+
+        // Exit to update progress bar
         if ((int) (*t / tf * 100) > progress_percentage)
         {   
             free(a);
@@ -178,20 +199,26 @@ void euler_cromer(
     real (*sol_state)[6 * objects_count]
 )
 {   
-    real (*a)[3] = malloc(3 * objects_count * sizeof(real));
+    real (*a)[3] = malloc(objects_count * 3 * sizeof(real));
 
+    // Current progress percentage rounded to int
     int progress_percentage = (int) round(*t / tf * 100);
+
+    // Main Loop
     for(int count = (int) round(*t / dt); count < npts; count++)
     {   
         acceleration(objects_count, x, a, m, G);
         for (int j = 0; j < objects_count; j++)
         {
+            // Calculation
             v[j][0] += a[j][0] * dt;
             v[j][1] += a[j][1] * dt;
             v[j][2] += a[j][2] * dt;
             x[j][0] += v[j][0] * dt;
             x[j][1] += v[j][1] * dt;
             x[j][2] += v[j][2] * dt;
+
+            // Store solution
             sol_state[count + 1][j * 3] = x[j][0];
             sol_state[count + 1][j * 3 + 1] = x[j][1];
             sol_state[count + 1][j * 3 + 2] = x[j][2];
@@ -199,8 +226,9 @@ void euler_cromer(
             sol_state[count + 1][objects_count * 3 + j * 3 + 1] = v[j][1];
             sol_state[count + 1][objects_count * 3 + j * 3 + 2] = v[j][2];
         }    
-            
         *t += dt;
+
+        // Exit to update progress bar
         if ((int) (*t / tf * 100) > progress_percentage)
         {   
             free(a);
@@ -222,26 +250,29 @@ void rk4(
     real (*sol_state)[6 * objects_count]
 )
 {
-    real (*temp_x)[3] = malloc(3 * objects_count * sizeof(real));
-    real (*temp_v)[3] = malloc(3 * objects_count * sizeof(real));
+    real (*temp_x)[3] = malloc(objects_count * 3 * sizeof(real));
+    real (*temp_v)[3] = malloc(objects_count * 3 * sizeof(real));
 
-    real (*a)[3] = malloc(3 * objects_count * sizeof(real));
+    real (*a)[3] = malloc(objects_count * 3 * sizeof(real));
 
-    real (*vk1)[3] = malloc(3 * objects_count * sizeof(real));
-    real (*vk2)[3] = malloc(3 * objects_count * sizeof(real));
-    real (*vk3)[3] = malloc(3 * objects_count * sizeof(real));
-    real (*vk4)[3] = malloc(3 * objects_count * sizeof(real));
-    real (*xk1)[3] = malloc(3 * objects_count * sizeof(real));
-    real (*xk2)[3] = malloc(3 * objects_count * sizeof(real));
-    real (*xk3)[3] = malloc(3 * objects_count * sizeof(real));
-    real (*xk4)[3] = malloc(3 * objects_count * sizeof(real));
+    real (*vk1)[3] = malloc(objects_count * 3 * sizeof(real));
+    real (*vk2)[3] = malloc(objects_count * 3 * sizeof(real));
+    real (*vk3)[3] = malloc(objects_count * 3 * sizeof(real));
+    real (*vk4)[3] = malloc(objects_count * 3 * sizeof(real));
+    real (*xk1)[3] = malloc(objects_count * 3 * sizeof(real));
+    real (*xk2)[3] = malloc(objects_count * 3 * sizeof(real));
+    real (*xk3)[3] = malloc(objects_count * 3 * sizeof(real));
+    real (*xk4)[3] = malloc(objects_count * 3 * sizeof(real));
 
+    // Current progress percentage rounded to int
     int progress_percentage = (int) round(*t / tf * 100);
+
+    // Main Loop
     for(int count = (int) round(*t / dt); count < npts; count++)
     {   
         acceleration(objects_count, x, a, m, G);
-        memcpy(vk1, a, 3 * objects_count * sizeof(real));
-        memcpy(xk1, v, 3 * objects_count * sizeof(real));
+        memcpy(vk1, a, objects_count * 3 * sizeof(real));
+        memcpy(xk1, v, objects_count * 3 * sizeof(real));
 
         for (int j = 0; j < objects_count; j++)
         {
@@ -253,8 +284,8 @@ void rk4(
             temp_v[j][2] = v[j][2] + 0.5 * vk1[j][2] * dt;
         }
         acceleration(objects_count, temp_x, a, m, G);
-        memcpy(vk2, a, 3 * objects_count * sizeof(real));
-        memcpy(xk2, temp_v, 3 * objects_count * sizeof(real));
+        memcpy(vk2, a, objects_count * 3 * sizeof(real));
+        memcpy(xk2, temp_v, objects_count * 3 * sizeof(real));
 
         for (int j = 0; j < objects_count; j++)
         {
@@ -266,8 +297,8 @@ void rk4(
             temp_v[j][2] = v[j][2] + 0.5 * vk2[j][2] * dt;
         }
         acceleration(objects_count, temp_x, a, m, G);
-        memcpy(vk3, a, 3 * objects_count * sizeof(real));
-        memcpy(xk3, temp_v, 3 * objects_count * sizeof(real));
+        memcpy(vk3, a, objects_count * 3 * sizeof(real));
+        memcpy(xk3, temp_v, objects_count * 3 * sizeof(real));
 
         for (int j = 0; j < objects_count; j++)
         {
@@ -279,18 +310,21 @@ void rk4(
             temp_v[j][2] = v[j][2] + vk3[j][2] * dt;
         }
         acceleration(objects_count, temp_x, a, m, G);
-        memcpy(vk4, a, 3 * objects_count * sizeof(real));
-        memcpy(xk4, temp_v, 3 * objects_count * sizeof(real));
+        memcpy(vk4, a, objects_count * 3 * sizeof(real));
+        memcpy(xk4, temp_v, objects_count * 3 * sizeof(real));
 
 
         for (int j = 0; j < objects_count; j++)
         {
+            // Calculation
             v[j][0] += (vk1[j][0] + 2 * vk2[j][0] + 2 * vk3[j][0] + vk4[j][0]) * dt / 6.0;
             v[j][1] += (vk1[j][1] + 2 * vk2[j][1] + 2 * vk3[j][1] + vk4[j][1]) * dt / 6.0;
             v[j][2] += (vk1[j][2] + 2 * vk2[j][2] + 2 * vk3[j][2] + vk4[j][2]) * dt / 6.0;
             x[j][0] += (xk1[j][0] + 2 * xk2[j][0] + 2 * xk3[j][0] + xk4[j][0]) * dt / 6.0;
             x[j][1] += (xk1[j][1] + 2 * xk2[j][1] + 2 * xk3[j][1] + xk4[j][1]) * dt / 6.0;
             x[j][2] += (xk1[j][2] + 2 * xk2[j][2] + 2 * xk3[j][2] + xk4[j][2]) * dt / 6.0;
+
+            // Store solution
             sol_state[count + 1][j * 3] = x[j][0];
             sol_state[count + 1][j * 3 + 1] = x[j][1];
             sol_state[count + 1][j * 3 + 2] = x[j][2];
@@ -298,8 +332,9 @@ void rk4(
             sol_state[count + 1][objects_count * 3 + j * 3 + 1] = v[j][1];
             sol_state[count + 1][objects_count * 3 + j * 3 + 2] = v[j][2];
         }    
-            
         *t += dt;
+
+        // Exit to update progress bar
         if ((int) (*t / tf * 100) > progress_percentage)
         {   
             free(a);
@@ -331,12 +366,15 @@ void leapfrog(
     real (*sol_state)[6 * objects_count]
 )
 {   
-    real (*a_0)[3] = malloc(3 * objects_count * sizeof(real));
-    real (*a_1)[3] = malloc(3 * objects_count * sizeof(real));
+    real (*a_0)[3] = malloc(objects_count * 3 * sizeof(real));
+    real (*a_1)[3] = malloc(objects_count * 3 * sizeof(real));
 
     int is_initialize = 1;
 
+    // Current progress percentage rounded to int
     int progress_percentage = (int) round(*t / tf * 100);
+
+    // Main Loop
     for(int count = (int) round(*t / dt); count < npts; count++)
     {       
         if (is_initialize == 1)
@@ -346,12 +384,13 @@ void leapfrog(
         }
         else 
         {
-            memcpy(a_0, a_1, 3 * objects_count * sizeof(real));
+            // Use a_1 from last iteration as a_0
+            memcpy(a_0, a_1, objects_count * 3 * sizeof(real));
         }
            
-
         for (int j = 0; j < objects_count; j++)
         {
+            // Calculation
             x[j][0] += v[j][0] * dt + 0.5 * a_0[j][0] * dt * dt;
             x[j][1] += v[j][1] * dt + 0.5 * a_0[j][1] * dt * dt;
             x[j][2] += v[j][2] * dt + 0.5 * a_0[j][2] * dt * dt;
@@ -359,6 +398,7 @@ void leapfrog(
         acceleration(objects_count, x, a_1, m, G);
         for (int j = 0; j < objects_count; j++)
         {
+            // Calculation
             v[j][0] += 0.5 * (a_0[j][0] + a_1[j][0]) * dt;
             v[j][1] += 0.5 * (a_0[j][1] + a_1[j][1]) * dt;
             v[j][2] += 0.5 * (a_0[j][2] + a_1[j][2]) * dt;
@@ -371,9 +411,9 @@ void leapfrog(
             sol_state[count + 1][objects_count * 3 + j * 3 + 1] = v[j][1];
             sol_state[count + 1][objects_count * 3 + j * 3 + 2] = v[j][2];
         }    
-            
         *t += dt;
-        
+
+        // Exit to update progress bar
         if ((int) (*t / tf * 100) > progress_percentage)
         {   
             free(a_0);
@@ -424,25 +464,25 @@ int rk_embedded(
 
     // Initialize arrays and values
     real sum, error, dt_new; 
-    real (*v_1)[3] = malloc(3 * objects_count * sizeof(real));
-    real (*x_1)[3] = malloc(3 * objects_count * sizeof(real));
+    real (*v_1)[3] = malloc(objects_count * 3 * sizeof(real));
+    real (*x_1)[3] = malloc(objects_count * 3 * sizeof(real));
     real *vk = malloc(stages * objects_count * 3 * sizeof(real));
     real *xk = malloc(stages * objects_count * 3 * sizeof(real));    
-    real (*temp_a)[3] = malloc(3 * objects_count * sizeof(real));
-    real (*temp_v)[3] = malloc(3 * objects_count * sizeof(real));
-    real (*temp_x)[3] = malloc(3 * objects_count * sizeof(real));
-    real (*error_estimation_delta_v)[3] = malloc(3 * objects_count * sizeof(real));
-    real (*error_estimation_delta_x)[3] = malloc(3 * objects_count * sizeof(real));
-    real (*tolerance_scale_v)[3] = malloc(3 * objects_count * sizeof(real));
-    real (*tolerance_scale_x)[3] = malloc(3 * objects_count * sizeof(real));
+    real (*temp_a)[3] = malloc(objects_count * 3 * sizeof(real));
+    real (*temp_v)[3] = malloc(objects_count * 3 * sizeof(real));
+    real (*temp_x)[3] = malloc(objects_count * 3 * sizeof(real));
+    real (*error_estimation_delta_v)[3] = malloc(objects_count * 3 * sizeof(real));
+    real (*error_estimation_delta_x)[3] = malloc(objects_count * 3 * sizeof(real));
+    real (*tolerance_scale_v)[3] = malloc(objects_count * 3 * sizeof(real));
+    real (*tolerance_scale_x)[3] = malloc(objects_count * 3 * sizeof(real));
 
-    
+    // Current progress percentage rounded to int
     int progress_percentage = (int) round(*t / tf * 100);
+
+    // Main Loop
     while (1)
     {
         // Calculate xk and vk
-
-        // vk0 and xk0
         acceleration(objects_count, x, temp_a, m, G);
         memcpy(vk, temp_a, objects_count * 3 * sizeof(real));
         memcpy(xk, v, objects_count * 3 * sizeof(real));
@@ -543,8 +583,8 @@ int rk_embedded(
         {
             // Advance step
             *t += *dt; 
-            memcpy(x, x_1, 3 * objects_count * sizeof(real));
-            memcpy(v, v_1, 3 * objects_count * sizeof(real));
+            memcpy(x, x_1, objects_count * 3 * sizeof(real));
+            memcpy(v, v_1, objects_count * 3 * sizeof(real));
             *count += 1;
 
             // Store step
@@ -578,8 +618,8 @@ int rk_embedded(
             } 
         }
 
+        // Calculate dt
         dt_new = *dt * safety_fac / pow(error, (1.0 / (1.0 + (real) min_power)));
-        // Prevent dt to be too small or too large relative to the last time step
         if (dt_new > safety_fac_max * *dt) 
         {
             *dt *= safety_fac_max;
@@ -603,6 +643,7 @@ int rk_embedded(
             *dt = tf - *t;
         }
         
+        // Exit to update progress bar
         if ((int) (*t / tf * 100) > progress_percentage)
         {
             free(error_estimation_delta_weights);
@@ -637,18 +678,4 @@ int rk_embedded(
             return 0;
         }
     }
-}
-
-
-
-real vec_norm(const real *vec, int vec_size)
-{   
-    real sum = 0.0;
-    if (vec_size == 3) 
-        sum = vec[0] * vec[0] + vec[1] * vec[1] + vec[2] * vec[2];
-    else
-    {
-        for (int i = 0; i < vec_size; i++) sum += vec[i] * vec[i];
-    }
-    return sqrt(sum);
 }
