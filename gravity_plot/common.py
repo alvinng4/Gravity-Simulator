648963f56c331f148bf8fdb382d842625f02b315
alvinng4 <alvinng324@gmail.com>
2024-05-13 10:04:48 +0800

    Plot: Refactored ask_user_permission to get_bool and update solar_3d.gif

--- gravity_plot/common.py
+++ gravity_plot/common.py
@@ -1,4 +1,26 @@
+import re
+
 import numpy as np
+
+
+def get_bool(msg: str) -> bool:
+    """
+    Get boolean input from user
+
+    Args: msg (str): Message to display to user
+    Display: "msg (y/n):"
+    Returns: bool
+    """
+    while True:
+        if matches := re.search(
+            r"^\s*(yes|no|y|n)\s*$", input(f"{msg} (y/n): "), re.IGNORECASE
+        ):
+            if matches.group(1).lower() in ["y", "yes"]:
+                return True
+            elif matches.group(1).lower() in ["n", "no"]:
+                return False
+
+        print("Invalid input. Please try again.\n")
 
 
 def acceleration(objects_count, x, m, G):
