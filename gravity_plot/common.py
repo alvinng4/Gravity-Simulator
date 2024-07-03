bea3df1e299b35dd9312c7b41587480cff219764
alvinng4 <alvinng324@gmail.com>
2024-07-03 11:25:31 +0800

    Simplify the numpy acceleration function
    Former-commit-id: 5b85badae298a2e8c7fe663158721aaae26cedcf [formerly b98f6da81ac684a2cc4df9899fe462380eb2f450]
    Former-commit-id: cff13bdd9c033e2ad6385ee91e4275ac395fd933

--- gravity_plot/common.py
+++ gravity_plot/common.py
@@ -112,17 +112,14 @@ def acceleration(objects_count, x, m, G):
     Calculate acceleration by a = - GM/r^3 vec{r}
     """
     # Allocate memory
-    temp_a = np.zeros((objects_count * objects_count, 3))
+    a = np.zeros((objects_count, 3))
 
     # Calculations
     for j in range(objects_count):
         for k in range(j + 1, objects_count):
             R = x[j] - x[k]
             temp_value = G * R / np.linalg.norm(R) ** 3
-            temp_a[j * objects_count + k] = -temp_value * m[k]
-            temp_a[k * objects_count + j] = temp_value * m[j]
-
-    temp_a = temp_a.reshape((objects_count, objects_count, 3))
-    a = np.sum(temp_a, axis=1)
+            a[j] += -temp_value * m[k]
+            a[k] += temp_value * m[j]
 
     return a
