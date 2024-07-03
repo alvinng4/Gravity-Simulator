15293afa307f3144531713adfeb8c86e6e11534e
alvinng4 <alvinng324@gmail.com>
2024-07-03 12:31:59 +0800

    Optimized acceleration function for numpy
    Former-commit-id: 37ab7731fd0bf6344fba83dbdc4f0544453b72c5 [formerly 625be62826498c25540ca58a9a9501c9d0551762]
    Former-commit-id: 898c565452e9da587a5e752d758cb3320c422ff8

--- gravity_sim/common.py
+++ gravity_sim/common.py
@@ -3,8 +3,9 @@ import numpy as np
 
 def acceleration(objects_count, x, m, G):
     """
-    Calculate acceleration by a = - GM/r^3 vec{r}
-    """
+    Calculate acceleration by a = - GM/r^3 vec{r} using a matrix approach
+
+    Old version of code using nested for loops:
     # Allocate memory
     a = np.zeros((objects_count, 3))
 
@@ -15,6 +16,25 @@ def acceleration(objects_count, x, m, G):
             temp_value = G * R / np.linalg.norm(R) ** 3
             a[j] += -temp_value * m[k]
             a[k] += temp_value * m[j]
+
+    return a
+    """
+    # Compute the displacement vector
+    r_ij = x[np.newaxis, :, :] - x[:, np.newaxis, :]
+
+    # Compute the distance squared
+    r_squared = np.sum(r_ij ** 2, axis=2)
+    softening = 1e-12   # To prevent r = 0
+    r_squared[r_squared < softening] = softening
+
+    # Compute 1/r^3
+    inv_r_cubed = r_squared ** (-1.5)
+
+    # Set diagonal elements to 0 to avoid self-interaction
+    np.fill_diagonal(inv_r_cubed, 0.0)
+
+    # Compute the acceleration
+    a = G * np.sum(r_ij * inv_r_cubed[:,:,np.newaxis] * m[np.newaxis, :, np.newaxis], axis=1)
 
     return a
 
