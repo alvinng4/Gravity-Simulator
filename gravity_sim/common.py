Fixing the calculation of npts
Former-commit-id: 3fa3efdb468bc90a1414e71eb0d25d559ba4db6a [formerly 3bca108a113a927c3bd0aa00e57d5a2118ec4cd2]
Former-commit-id: 1d2f4a85bde77286c2e118823047dd8f015e11f8

gravity_sim/common.py
@@ -1,5 +1,6 @@
 import csv
 import datetime
+import math
 from pathlib import Path
 import re
 import time
@@ -714,7 +715,7 @@ def trim_data(
         raise ValueError("store_every_n must be greater than 1")
 
     input_data_shape = list(data.shape)
-    input_data_shape[0] = original_data_size // store_every_n
+    input_data_shape[0] = math.ceil(original_data_size / store_every_n)
     trimmed_data = np.zeros(tuple(input_data_shape))
 
     store_count = 0